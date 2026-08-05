0c9b4d81bc749743fc7275bfa61ebfc12cc45e0e
Jomar <ruarjomar03@gmail.com>
2026-08-05 15:19:24 +0800

    fix: update favicon links in app.blade.php

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -31,9 +31,8 @@
             }
         </style>
 
-        <link rel="icon" href="/favicon.ico" sizes="any">
-        <link rel="icon" href="/favicon.svg" type="image/svg+xml">
-        <link rel="apple-touch-icon" href="/apple-touch-icon.png">
+        <link rel="icon" href="/logos.png" sizes="any">
+        <link rel="apple-touch-icon" href="/logos.png">
 
         @fonts
 
